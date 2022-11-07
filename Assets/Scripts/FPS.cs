54ae0ab8e250fcc99233ebe649555df52a0c6bb1
kalypa <80308782+kalypa@users.noreply.github.com>
2022-11-07 15:43:07 +0900

    Prototype

--- Assets/Scripts/FPS.cs
+++ Assets/Scripts/FPS.cs
@@ -18,7 +18,7 @@ public class FPS : MonoBehaviour
 
         Rect rect = new Rect(0, 0, w, h * 2 / 100);
         style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 100;
+        style.fontSize = h * 2 / 50;
         style.normal.textColor = new Color(1, 1, 1, 1.0f);
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
