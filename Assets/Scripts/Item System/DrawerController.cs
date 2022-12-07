b10a34479958181ab56f2a27dfdfff7790e1d25a
kalypa <80308782+kalypa@users.noreply.github.com>
2022-12-07 20:06:49 +0900

    버그 수정

--- Assets/Scripts/Item System/DrawerController.cs
+++ Assets/Scripts/Item System/DrawerController.cs
@@ -59,11 +59,13 @@ public class DrawerController : MonoBehaviour
 
     public void DrawerOpenSound()
     {
+        Debug.Log("Open");
         AudioManager.instance.Play(drawerOpenSound);
     }
 
     public void DrawerCloseSound()
     {
+        Debug.Log("Close");
         AudioManager.instance.Play(drawerCloseSound);
     }
 }
