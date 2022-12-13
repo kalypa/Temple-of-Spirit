d7c7a6ccded37bbf73cc94ff7cff2c383711ec41
kalypa <80308782+kalypa@users.noreply.github.com>
2022-12-13 09:27:26 +0900

    텍스트 수정

--- Assets/Scripts/Player/TutorialText.cs
+++ Assets/Scripts/Player/TutorialText.cs
@@ -7,9 +7,8 @@ public class TutorialText : SingleMonobehaviour<TutorialText>
 {
     public Text textUI;
     public string TutorialStoryText = "찢어지게 가난해서 힘들게 살아가던 나는 근처 산속 사원에 엄청나게 " +
-        "값비싼 \r\n신물이 있다는 소문을 들었다. 그 소문에 홀려 깊은 산속에 들어가 길을 잃은 \r\n나는 " +
+        "값비싼 \r\n신물이 있다는 소문을 들었다. 그 소문에 홀려 깊은 산속에 들어가 길을 잃었지만... \r\n나는 " +
         "어째서인지 사원에 도착해 있었다...";
-    // Start is called before the first frame update
     
     public IEnumerator OnType(float interval, string Say)
     {
